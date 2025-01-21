fdb571a2954b0846d41321fb0f3483c85f6bcf50
Rudy Villatoro <rdvp86@hotmail.com>
2025-01-20 20:31:46 -0600

    Add changes to display random messages in user's screen

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-const networkProtocolsConcepts = ["TCP/IP", "HTTP/HTTPS", "FTP", "SMTP", "Network Segmentation", "IMAP", "SSH", "Subnetting", "Port Forwarding"];
-const programmingConcepts = ["Memory Leak", "Functional Programming", "Design Patterns", "Staging Environment in GIT", "Parse", "Recursion", "Big O Notation", "RESTful APIs"];
-const networkProgrammingTools = ["Wireshark", "Nmap", "Netcat", "Cain & Abel", "Metasploit", "Burp Suite", "Kismet", "Aircrack-ng"];
+const networkProtocolsConcepts = [
+  "TCP/IP",
+  "HTTP/HTTPS",
+  "FTP",
+  "SMTP",
+  "Network Segmentation",
+  "IMAP",
+  "SSH",
+  "Subnetting",
+  "Port Forwarding",
+];
+const programmingConcepts = [
+  "Memory Leak",
+  "Functional Programming",
+  "Design Patterns",
+  "Staging Environment in GIT",
+  "Parse",
+  "Recursion",
+  "Big O Notation",
+  "RESTful APIs",
+];
+const networkProgrammingTools = [
+  "Wireshark",
+  "Nmap",
+  "Netcat",
+  "Cain & Abel",
+  "Metasploit",
+  "Burp Suite",
+  "Kismet",
+  "Aircrack-ng",
+];
 
-const randomConcept = arrOfConcepts => {
-    const randomSelection = arrOfConcepts[Math.floor(Math.random() * arrOfConcepts.length)];
-    return randomSelection;
-}
+const randomConcept = (arrOfConcepts) => {
+  const randomSelection =
+    arrOfConcepts[Math.floor(Math.random() * arrOfConcepts.length)];
+  return randomSelection;
+};
 
-const networkProtocolSelected = `Can you explain the network protocol ${randomConcept(networkProtocolsConcepts)}?`;
-const programmingConceptSelected = `What is ${randomConcept(programmingConcepts)}?`;
-const networkProgrammingToolSelected = `What is ${randomConcept(networkProgrammingTools)} used for?`;
+const networkProtocolSelected = `Can you explain the network protocol ${randomConcept(
+  networkProtocolsConcepts
+)}?`;
+const programmingConceptSelected = `What is ${randomConcept(
+  programmingConcepts
+)}?`;
+const networkProgrammingToolSelected = `What is ${randomConcept(
+  networkProgrammingTools
+)} used for?`;
 
-console.log(networkProtocolSelected);
-console.log(programmingConceptSelected);
-console.log(networkProgrammingToolSelected);
+const triggerButton = document.querySelector("button");
+triggerButton.addEventListener("click", () => {
+  const buttonContainer = document.querySelector(".container");
+  const messagesContainer = document.createElement("div");
+  messagesContainer.classList.add("messages");
+  messagesContainer.innerHTML = `
+        <h2>${networkProtocolSelected}</h2>
+        <h2>${programmingConceptSelected}</h2>
+        <h2>${networkProgrammingToolSelected}</h2>
+    `;
+  //document.body.appendChild(messagesContainer);
+  buttonContainer.replaceWith(messagesContainer);
+});
